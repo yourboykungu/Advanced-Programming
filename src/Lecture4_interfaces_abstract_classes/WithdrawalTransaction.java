d6a6c84d823706a81e3bda0a281d12a1d2e32dfa
yourboykungu <badusvanmark@gmail.com>
2024-11-30 03:13:49 +0300

    final assignment

--- src/Lecture4_interfaces_abstract_classes/WithdrawalTransaction.java
+++ src/Lecture4_interfaces_abstract_classes/WithdrawalTransaction.java
@@ -1,45 +1,52 @@
 package Lecture4_interfaces_abstract_classes;
 
 import org.jetbrains.annotations.NotNull;
-
 import java.util.Calendar;
 
 public class WithdrawalTransaction extends BaseTransaction {
+
     public WithdrawalTransaction(int amount, @NotNull Calendar date) {
         super(amount, date);
     }
 
-    private boolean checkDepositAmount(int amt) {
-        if (amt < 0) {
-            return false;
-        } else {
-            return true;
-        }
+    private boolean checkWithdrawalAmount(double amt) {
+        return amt >= 0;
     }
-
-    // Method to reverse the transaction
-    public boolean reverse() {
-        return true;
-    } // return true if reversal was successful
-
-    // Method to print a transaction receipt or details
-    public void printTransactionDetails() {
-        System.out.println("Deposit Trasaction: " + this.toString());
-    }
-
-    /*
-    Oportunity for assignment: implementing different form of withdrawal
-     */
+    @Override
     public void apply(BankAccount ba) {
-        double curr_balance = ba.getBalance();
-        if (curr_balance > getAmount()) {
-            double new_balance = curr_balance - getAmount();
-            ba.setBalance(new_balance);
+        apply(ba, "Standard Withdrawal"); // Default to "Standard Withdrawal" type
+    }
+
+    public void apply(BankAccount ba,  String withdrawalType) {
+        double currentBalance = ba.getBalance();
+
+        if (!checkWithdrawalAmount(getAmount())) {
+            System.out.println("Invalid withdrawal amount.");
+            return;
+        }
+
+        if (currentBalance >= getAmount()) {
+            double newBalance = currentBalance - getAmount();
+            ba.setBalance(newBalance);
+            System.out.println("Withdrawal of " + getAmount() + " applied successfully.");
+        } else {
+            System.out.println("Insufficient funds for withdrawal.");
         }
     }
 
-    /*
-    Assignment 1 Q3: Write the Reverse method - a method unique to the WithdrawalTransaction Class
-     */
-}
+    public void printTransactionDetails() {
+        System.out.println("Withdrawal Transaction:");
+        System.out.println("Amount: " + getAmount());
+        System.out.println("Date: " + getDate().getTime());
+        System.out.println("Transaction ID: " + getTransactionID());
+    }
 
+    // Reverse method to create a ReversalTransaction
+    public ReversalTransaction reverse(BankAccount ba) {
+        double currentBalance = ba.getBalance();
+        ba.setBalance(currentBalance + getAmount());
+
+        // Create a reversal transaction with a double amount
+        return new ReversalTransaction(this.getAmount(), this.getDate());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Lecture1_adt.*; // Import all classes from Lecture1_adt package to be used in this client code
+import Lecture4_interfaces_abstract_classes.*;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -44,7 +45,6 @@ public class Main {
         Calendar d1 = new GregorianCalendar();
 
         Lecture1_adt.Transaction2 t = new Lecture1_adt.Transaction2(1000, d1);
-
         Lecture1_adt.Transaction2 modified_t = makeNextPayment(t);
 
         System.out.println("\n\nState of the Object T1 After Client Code Tried to Change the Amount");
@@ -70,8 +70,6 @@ public class Main {
 
         List<Transaction3> listOfTransaction3s = new ArrayList<Transaction3>();
         Calendar date = new GregorianCalendar(2024, Calendar.JANUARY, 3);
-
-
         for (int i = 0; i < 12; i++) {
             listOfTransaction3s.add(new Transaction3(amount, date));
             date.add(Calendar.MONTH, 1);
@@ -83,11 +81,8 @@ public class Main {
     Testing Transaction3 class
      */
     public static void testTransaction3() {
-
         List<Transaction3> allPaymentsIn2024 = makeYearOfPayments(1000);
-
         for (Transaction3 t3 : allPaymentsIn2024) {
-
             // Display all the 12 Transactions
             for (Transaction3 transact : allPaymentsIn2024) {
                 System.out.println("\n\n  ::::::::::::::::::::::::::::::::::::::::::::\n");
@@ -114,7 +109,6 @@ public class Main {
         List<Transaction4> listOfTransaction4s = new ArrayList<Transaction4>();
         Calendar date = new GregorianCalendar(2024, Calendar.JANUARY, 3);
 
-
         for (int i = 0; i < 12; i++) {
             listOfTransaction4s.add(new Transaction4(amount, date));
             date.add(Calendar.MONTH, 1);
@@ -126,21 +120,46 @@ public class Main {
     Testing Transaction3 class
      */
     public static void testTransaction4() {
-
-        /*
-         * Call the function to make all the Twelve transaction in a year of our business
-         */
-
         List<Transaction4> transactionsIn2024 = makeYearOfPaymentsFinal(1200);
-
-        // Display all the 12 Transactions
         for (Transaction4 transact : transactionsIn2024) {
             System.out.println("\n\n  ::::::::::::::::::::::::::::::::::::::::::::\n");
             System.out.println("Lecture1_adt.TransactionInterface Amount: \t "+transact.getAmount());
             System.out.println("Lecture1_adt.TransactionInterface Date: \t "+transact.getDate().getTime());
         }
+    }
+    // New method to test Withdrawal and Reversal Transactions with BankAccount
+    public static void testWithdrawalAndReversal() {
+        // Create a BankAccount with an initial balance of 5000
+        BankAccount bankAccount = new BankAccount(5000);
+        bankAccount.printBalance(); // Initial balance
 
-        // Please Take a look at all the 12 transaction now and compare with the outputs of the Transaction3 class
+        // Create and apply a standard withdrawal
+        Calendar d1 = new GregorianCalendar();
+        WithdrawalTransaction standardWithdrawal = new WithdrawalTransaction(1000, d1);
+        standardWithdrawal.apply(bankAccount, "Standard Withdrawal");
+        System.out.println("\nAfter Standard Withdrawal:");
+        bankAccount.printBalance(); // Balance after standard withdrawal
+
+        // Create and apply an ATM withdrawal
+        WithdrawalTransaction atmWithdrawal = new WithdrawalTransaction(500, d1);
+        atmWithdrawal.apply(bankAccount, "ATM Withdrawal");
+        System.out.println("\nAfter ATM Withdrawal:");
+        bankAccount.printBalance(); // Balance after ATM withdrawal
+
+        // Create and apply an online withdrawal
+        WithdrawalTransaction onlineWithdrawal = new WithdrawalTransaction(200, d1);
+        onlineWithdrawal.apply(bankAccount, "Online Withdrawal");
+        System.out.println("\nAfter Online Withdrawal:");
+        bankAccount.printBalance(); // Balance after online withdrawal
+
+
+        // Create a reversal of the withdrawal transaction
+        ReversalTransaction reversal = standardWithdrawal.reverse(bankAccount);
+
+        // Apply the reversal transaction to the account
+        bankAccount.applyTransaction(reversal);
+        System.out.println("\nAfter Reversal:");
+        bankAccount.printBalance(); // Balance after reversal
     }
 
 
@@ -148,9 +167,11 @@ public class Main {
         // This is the client code
         // Uncomment the following lines to test the class which you would like to test
 
-        // testTransaction1()
+        //testTransaction1();
         // testTransaction2()
         // testTransaction3()
         // testTransaction4()
+
+        testWithdrawalAndReversal();
     }
 }
